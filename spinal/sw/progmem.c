eac43a0574e921b4ac908567881ca6a566597add
Tom Verbeure <hombre+github@gmail.com>
2018-12-29 15:07:57 -0800

    Refactor dvi code.

--- spinal/sw/progmem.c
+++ spinal/sw/progmem.c
@@ -6,6 +6,7 @@
 #include "print.h"
 #include "i2c.h"
 #include "gmii.h"
+#include "dvi.h"
 
 static inline uint32_t rdcycle(void) {
     uint32_t cycle;
@@ -42,53 +43,6 @@ int button_pressed()
     return REG_RD(BUTTON) == 0x01;
 }
 
-i2c_ctx_t dvi_ctrl_i2c_ctx;
-
-void dvi_ctrl_init()
-{
-    dvi_ctrl_i2c_ctx.base_addr = 0;
-    dvi_ctrl_i2c_ctx.scl_pin_nr = 0;
-    dvi_ctrl_i2c_ctx.sda_pin_nr = 1;
-
-    i2c_init(&dvi_ctrl_i2c_ctx);
-
-    byte chrontel_registers[][2] = {
-        {   0x1c,       0x00},              // 1x DDR clocking
-        {   0x1d,       0x40},              // internal clock delay
-        {   0x1f,       0x80},              // input data format. Bit 4: vsp, bit 4: hsp
-
-#if 0
-        // clk <= 65Mhz
-        {   0x33,       0x08},              // charge pump settings. See table 10
-        {   0x34,       0x16},              // charge pump settings. See table 10
-        {   0x36,       0x60},              // charge pump settings. See table 10
-#else
-        // clk > 65Mhz
-        {   0x33,       0x06},              // charge pump settings. See table 10
-        {   0x34,       0x26},              // charge pump settings. See table 10
-        {   0x36,       0xa0},              // charge pump settings. See table 10
-#endif
-
-        {   0x48,       0x18},              // Test Image: none
-//        {   0x48,       0x19},              // Test Image: color bars
-//        {   0x48,       0x1e},              // Test Image: luminance ramp
-        {   0x49,       0xc0},              // DVI on
-
-        {   0xff,       0xff}               // The end
-    };
-
-    int i=0;
-    while(chrontel_registers[i][0] != 0xff){
-        byte reg_nr  = chrontel_registers[i][0];
-        byte reg_val = chrontel_registers[i][1];
-
-        i2c_write_regs(&dvi_ctrl_i2c_ctx, 0x75<<1, reg_nr, &reg_val, 1);
-        i2c_write_regs(&dvi_ctrl_i2c_ctx, 0x76<<1, reg_nr, &reg_val, 1);
-
-        ++i;
-    }
-}
-
 int main() {
 
     REG_WR(LED_DIR, 0xff);
@@ -101,26 +55,6 @@ int main() {
         wait(WAIT_CYCLES);
         REG_WR(LED_WRITE, 0x02);
         wait(WAIT_CYCLES);
-    }
-#endif
-
-
-#if 0
-    int addr = 0x75;
-    while(1){
-        {
-            bool ack = i2c_write_reg_nr(&dvi_ctrl_i2c_ctx, (addr & 0x7f)<<1, 0x5a);
-            if (ack){
-                REG_WR(LED_WRITE, 0xff);
-                wait(10000);
-            }
-            else{
-                REG_WR(LED_WRITE, 0x00);
-            }
-            wait(250);
-        }
-
-        addr ^= 0x03;
     }
 #endif
 
