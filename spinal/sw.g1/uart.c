473e17cd7578cb4780d2ac3129a5ba36277c0ca7
Skip Hansen <skip@gfrn.org>
2019-01-23 18:40:39 -0800

    Debug UART added (output to blue LED), print redirected to UART.

---
+++ spinal/sw.g1/uart.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+
+#include "reg.h"
+#include "top_defines.h"
+
+
+void UartInit()
+{
+   union {
+      UartFraming Framing;
+      uint32_t uint32;
+   } u;
+
+// baudrate = Fclk / (rxSamplePerBit*clockDividerWidth)
+
+// Core clock = 25 Mhz, bits / baud = 8, baudrate = 115200
+// div = 27.13
+   REG_WR(UART_CLK_DIV,27);
+
+// NB: apparently there's a bug in Spinal 1.2.2's Uart, we really get 7 data
+// bits...
+   u.Framing.DataLength = 6;  
+
+   u.Framing.Parity = PARITY_NONE;
+   u.Framing.Stop = 0;
+
+   REG_WR(UART_FRAME,u.uint32);
+}
+
+void UartPutch(char c)
+{
+// For debugging with a big fifo don't bother waiting for FIFO empty
+// while ((REG_RD(UART_STATUS) & 0x00FF0000) == 0);
+   REG_WR(UART_TX_DATA,c);
+}
+
---
+++ spinal/sw.g1/uart.h
@@ -0,0 +1,8 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+void UartInit(void);
+void UartPutch(char c);
+
+#endif   // _UART_H_
+
