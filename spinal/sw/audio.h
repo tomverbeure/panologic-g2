0ad9c45f0e7e8de27b44490bdbf459b2bbf66b35
Skip Hansen <skip@gfrn.org>
2019-10-27 19:03:43 -0700

    Generalize i2c so it can be used for the codec and DDC as well as DVI. (#13)
    * Generalize i2c so it can be used for the codec and DDC as well as DVI.
    
    * Revert change to Pano.ucf for audio, we're not ready for that yet.

---
+++ spinal/sw/audio.h
@@ -0,0 +1,43 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#define WM8750_LIN_VOL_ADDR                     0x00
+#define WM8750_RIN_VOL_ADDR                     0x01
+#define WM8750_LOUT1_VOL_ADDR                   0x02
+#define WM8750_ROUT1_VOL_ADDR                   0x03
+#define WM8750_ADC_DAC_CTRL_ADDR                0x05
+#define WM8750_AUDIO_INTFC_ADDR                 0x07
+#define WM8750_SAMPLE_RATE_ADDR                 0x08
+#define WM8750_LCHAN_VOL_ADDR                   0x0A
+#define WM8750_RCHAN_VOL_ADDR                   0x0B
+#define WM8750_BASS_CTRL_ADDR                   0x0C
+#define WM8750_TREBLE_CTRL_ADDR                 0x0D
+#define WM8750_RESET_ADDR                       0x0F
+#define WM8750_3D_CTRL_ADDR                     0x10
+#define WM8750_ALC1_ADDR                        0x11
+#define WM8750_ALC2_ADDR                        0x12
+#define WM8750_ALC3_ADDR                        0x13
+#define WM8750_NOISE_GATE_ADDR                  0x14
+#define WM8750_LADC_VOL                         0x15
+#define WM8750_RADC_VOL                         0x16
+#define WM8750_ADDITIONAL_CTRL1_ADDR            0x17
+#define WM8750_ADDITIONAL_CTRL2_ADDR            0x18
+#define WM8750_PWR_MANAGEMENT1_ADDR             0x19
+#define WM8750_PWR_MANAGEMENT2_ADDR             0x1a
+#define WM8750_ADDITIONAL_CTRL3_ADDR            0x1b
+#define WM8750_ADC_IN_MODE_ADDR                 0x1f
+#define WM8750_ADCL_SIGNAL_PATH_ADDR            0x20
+#define WM8750_ADCR_SIGNAL_PATH_ADDR            0x21
+#define WM8750_LEFT_MIXER_CTRL1_ADDR            0x22
+#define WM8750_LEFT_MIXER_CTRL2_ADDR            0x23
+#define WM8750_RIGHT_MIXER_CTRL1_ADDR           0x24
+#define WM8750_RIGHT_MIXER_CTRL2_ADDR           0x25
+#define WM8750_MONO_MIXER_CTRL1_ADDR            0x26
+#define WM8750_MONO_MIXER_CTRL2_ADDR            0x27
+#define WM8750_LOUT2_VOL_ADDR                   0x28
+#define WM8750_ROUT2_VOL_ADDR                   0x29
+#define WM8750_MONOOUT_VOL_ADDR                 0x2A
+
+void audio_init();
+
+#endif
--- spinal/sw/top_defines.h
+++ spinal/sw/top_defines.h
@@ -1,15 +1,29 @@
 
 #define LED_READ_ADDR               0x00000000
 #define LED_WRITE_ADDR              0x00000004
-#define	LED_DIR_ADDR                0x00000008
+#define  LED_DIR_ADDR                0x00000008
 
 #define BUTTON_ADDR                 0x00000010
 
-#define DVI_CTRL_DIR_ADDR           0x00000100
-#define DVI_CTRL_WR_ADDR            0x00000104
-#define DVI_CTRL_SET_ADDR           0x00000108
-#define DVI_CTRL_CLR_ADDR           0x0000010c
-#define DVI_CTRL_RD_ADDR            0x00000110
+// object CCGpio
+// 0x0000 : Direction. 0 -> input, 1 -> output
+// 0x0004 : Write
+// 0x0008 : Set
+// 0x000c : Clear
+// 0x0010 : Read
+#define CCGPIO_DIR_OFFSET           0
+#define CCGPIO_WR_OFFSET            4
+#define CCGPIO_SET_OFFSET           8
+#define CCGPIO_CLR_OFFSET           0xc
+#define CCGPIO_RD_OFFSET            0x10
+
+// val u_dvi_ctrl = CCGpio(2)
+#define DVI_CTRL_BASE_ADDR          0x00000100
+#define DVI_CTRL_DIR_ADDR           (DVI_CTRL_BASE_ADDR + CCGPIO_DIR_OFFSET)
+#define DVI_CTRL_WR_ADDR            (DVI_CTRL_BASE_ADDR + CCGPIO_WR_OFFSET)
+#define DVI_CTRL_SET_ADDR           (DVI_CTRL_BASE_ADDR + CCGPIO_SET_OFFSET)
+#define DVI_CTRL_CLR_ADDR           (DVI_CTRL_BASE_ADDR + CCGPIO_CLR_OFFSET)
+#define DVI_CTRL_RD_ADDR            (DVI_CTRL_BASE_ADDR + CCGPIO_RD_OFFSET)
 
 #define GMII_MDIO_ADDR              0x00010000
 #define GMII_MDC_VAL_BIT                0
